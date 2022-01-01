ea9b3b70ae9c7a119e95f2667d6d1abd6d561f2f
Victor Rodionov <v.a.rodionov@gmail.com>
2022-01-02 03:18:34 +0400

    Added Stack

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Stack;
+
 Console.WriteLine("Hello, World!");
+
+MyStack stack = new MyStack();
+
+stack.Push(12);
+stack.Push(44);
+stack.Push("Data");
+
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+
+
 
---
+++ Stack/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Stack
+{
+    
+
+    public class MyStack
+    {
+        private Cell? head;
+
+        public void Push(object data)
+        {
+            if (head == null)
+            {
+                head = new Cell(data);
+            }
+            else
+            {
+                var next = head;
+                head = new Cell(data);
+                head.Next = next;
+            }
+        }
+
+        public object Pop()
+        {
+            object? data = null;
+            if (head == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                data = head.Data;
+                head = head.Next;
+            }
+
+            return data;
+        }
+
+        private class Cell
+        {
+            public Cell(object data)
+            {
+                Data = data;
+            }
+
+            public object Data { get; private set; }
+            public Cell Next { get; set; }
+        }
+    }
+}
+
