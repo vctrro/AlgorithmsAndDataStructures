04b86be88b163cf6a5f2ed1b73ecc230ffdf6f6c
Victor Rodionov <vctr.ro@gmail.com>
2022-01-16 18:54:32 +0400

    Added Sorting string by length

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -6,16 +6,41 @@ namespace MergeSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] intData = { 1, 2, 5, 7, 12, 24, 76, 0, 23, 56, 7, 44, 3, 22, 1, 34, 809, 3};
+            double[] doubData = { 1.4, 2.13, 5.3, 7.42431, 12.12, 12.24, 12.121276, 0.1,
+                12.121223, 12.121256, 12.12127, 12.1244, 3, 12.1222, 1, 12.1234, 12.809, 3.14};
+            string[] strData = { "1", "2", "asd5", "7sd", "qwe'12", " qwe'1 2", "qwe '12", "_24", "qwe'1 2", "qwe '12", "q we'12" };
 
-            int[] intData = { 1, 2, 5, 7, 12, 24, 76, 0, 23, 56, 7, 44, 3, 22, 1, 34 };
-            string[] stringData = { "1", "2", "asd5", "7sd", "qwe'12", "_24" };
+            Console.WriteLine("Sort Int data");
 
-            var result = Sort.MergeSort(intData);
-
-            foreach (var item in result)
+            foreach (var item in Sort.MergeSort(intData))
             {
                 Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Sort Double data");
+
+            foreach (var item in Sort.MergeSort(doubData))
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Sort String data");
+
+            foreach (var item in Sort.MergeSort(strData))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Sort String data by length");
+
+            foreach (var item in Sort.MergeSortByLength(strData))
+            {
+                Console.WriteLine(item);
             }
         }
     }
--- MergeSort/Sort.cs
+++ MergeSort/Sort.cs
@@ -53,5 +53,70 @@ namespace MergeSort
 
             return result;
         }
+
+        public static string[] MergeSortByLength(string[] unsortData)
+        {
+            int length = unsortData.Length;
+            if (length <= 1) return unsortData;
+
+            int leftSize = length / 2;
+            int rightSize = length - leftSize;
+            string[] left, right, result;
+
+            left = new string[leftSize];
+            right = new string[rightSize];
+
+            for (int i = 0; i < leftSize; i++)
+                left[i] = unsortData[i];
+            for (int i = 0; i < rightSize; i++)
+                right[i] = unsortData[i + leftSize];
+
+            left = MergeSortByLength(left);
+            right = MergeSortByLength(right);
+
+            result = new string[length];
+
+            int lIndex = 0, rIndex = 0;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (lIndex >= left.Length)
+                {
+                    result[i] = right[rIndex];
+                    rIndex++;
+                }
+                else if (rIndex >= right.Length)
+                {
+                    result[i] = left[lIndex];
+                    lIndex++;
+                }
+                else if (left[lIndex].Length < right[rIndex].Length)
+                {
+                    result[i] = left[lIndex];
+                    lIndex++;
+                }
+                else if (left[lIndex].Length == right[rIndex].Length)
+                {
+                    //if lengths are equal, sort by value 
+                    if (left[lIndex].CompareTo(right[rIndex]) <= 0)
+                    {
+                        result[i] = left[lIndex];
+                        lIndex++;
+                    }
+                    else
+                    {
+                        result[i] = right[rIndex];
+                        rIndex++;
+                    }
+                }
+                else
+                {
+                    result[i] = right[rIndex];
+                    rIndex++;
+                }
+            }
+
+            return result;
+        }
     }
 }
